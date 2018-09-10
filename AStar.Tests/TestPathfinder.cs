27acc6931d652ee6e7a86537372cb51fdbd5c72e
val <val.antonini@gmail.com>
2018-09-10 18:51:56 +1000

    - nunit tests

--- AStar.Tests/TestPathfinder.cs
+++ AStar.Tests/TestPathfinder.cs
@@ -1,81 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using Xunit;
-using FluentAssertions;
+using NUnit.Framework;
+using Shouldly;
 
 namespace AStar.Tests
 {
+    [TestFixture]
     public class TestPathfinder
     {
         private byte[,] _grid;
         private PathFinder _pathFinder;
 
+        [SetUp]
         public void SetUp()
         {
             _grid = CreateMatrix(8);
             _pathFinder = new PathFinder(_grid);
         }
 
-        [Fact]
+        [Test]
         public void ShouldPathToSelf()
         {
-            SetUp();
             var path = _pathFinder.FindPath(new Point(1, 1), new Point(1, 1));
-            path.Count.Should().Be(1);
+            path.Count.ShouldBe(1);
             
             var node = path[0];
-            node.X.Should().Be(1);
-            node.Y.Should().Be(1);
+            node.X.ShouldBe(1);
+            node.Y.ShouldBe(1);
         }
 
-        [Fact]
+        [Test]
         public void ShouldPathToAdjacent()
         {
-            SetUp();
             var path = _pathFinder.FindPath(new Point(1, 1), new Point(2, 1));
             
-            path.Count.Should().Be(2);
+            path.Count.ShouldBe(2);
 
             var node = path[0];
             
-            node.X.Should().Be(2);
-            node.Y.Should().Be(1);
+            node.X.ShouldBe(2);
+            node.Y.ShouldBe(1);
 
             node = path[1];
-            node.X.Should().Be(1);
-            node.Y.Should().Be(1);
+            node.X.ShouldBe(1);
+            node.Y.ShouldBe(1);
         }
 
-        [Fact]
+        [Test]
         public void ShouldDoSimplePath()
         {
-            SetUp();
             var path = _pathFinder.FindPath(new Point(1, 1), new Point(4, 2));
             Helper.Print(_grid, path);
-            path.Count.Should().Be(4);
+            path.Count.ShouldBe(4);
 
             var item = path[3];
-            item.X.Should().Be(1);
-            item.Y.Should().Be(1);
+            item.X.ShouldBe(1);
+            item.Y.ShouldBe(1);
 
             item = path[2];
-            item.X.Should().Be(2);
-            item.Y.Should().Be(2);
+            item.X.ShouldBe(2);
+            item.Y.ShouldBe(2);
 
             item = path[1];
-            item.X.Should().Be(3);
-            item.Y.Should().Be(2);
+            item.X.ShouldBe(3);
+            item.Y.ShouldBe(2);
 
             item = path[0];
-            item.X.Should().Be(4);
-            item.Y.Should().Be(2);
+            item.X.ShouldBe(4);
+            item.Y.ShouldBe(2);
         }
 
-        [Fact]
+        [Test]
         public void ShouldDoSimplePathWithNoDiagonal()
         {
-            SetUp();
             var pathfinderOptions = new PathFinderOptions { Diagonals = false};
             _pathFinder = new PathFinder(_grid, pathfinderOptions);
 
@@ -83,33 +81,32 @@ namespace AStar.Tests
             Helper.Print(_grid, path);
             PrintCoordinates(path);
 
-            path.Count.Should().Be(5);
+            path.Count.ShouldBe(5);
 
             var item = path[4];
-            item.X.Should().Be(1);
-            item.Y.Should().Be(1);
+            item.X.ShouldBe(1);
+            item.Y.ShouldBe(1);
 
             item = path[3];
-            item.X.Should().Be(2);
-            item.Y.Should().Be(1);
+            item.X.ShouldBe(2);
+            item.Y.ShouldBe(1);
             
             item = path[2];
-            item.X.Should().Be(3);
-            item.Y.Should().Be(1);
+            item.X.ShouldBe(3);
+            item.Y.ShouldBe(1);
             
             item = path[1];
-            item.X.Should().Be(4);
-            item.Y.Should().Be(1);
+            item.X.ShouldBe(4);
+            item.Y.ShouldBe(1);
             
             item = path[0];
-            item.X.Should().Be(4);
-            item.Y.Should().Be(2);
+            item.X.ShouldBe(4);
+            item.Y.ShouldBe(2);
         }
 
-        [Fact]
+        [Test]
         public void ShouldDoSimplePathWithNoDiagonalAroundObstacle()
         {
-            SetUp();
             var pathfinderOptions = new PathFinderOptions { Diagonals = false };
             _pathFinder = new PathFinder(_grid, pathfinderOptions);
 
@@ -121,35 +118,35 @@ namespace AStar.Tests
             Helper.Print(_grid, path);
             PrintCoordinates(path);
 
-            path.Count.Should().Be(7);
+            path.Count.ShouldBe(7);
 
             var item = path[6];
-            item.X.Should().Be(1);
-            item.Y.Should().Be(1);
+            item.X.ShouldBe(1);
+            item.Y.ShouldBe(1);
 
             item = path[5];
-            item.X.Should().Be(1);
-            item.Y.Should().Be(2);
+            item.X.ShouldBe(1);
+            item.Y.ShouldBe(2);
 
             item = path[4];
-            item.X.Should().Be(1);
-            item.Y.Should().Be(3);
+            item.X.ShouldBe(1);
+            item.Y.ShouldBe(3);
 
             item = path[3];
-            item.X.Should().Be(2);
-            item.Y.Should().Be(3);
+            item.X.ShouldBe(2);
+            item.Y.ShouldBe(3);
 
             item = path[2];
-            item.X.Should().Be(3);
-            item.Y.Should().Be(3);
+            item.X.ShouldBe(3);
+            item.Y.ShouldBe(3);
 
             item = path[1];
-            item.X.Should().Be(3);
-            item.Y.Should().Be(2);
+            item.X.ShouldBe(3);
+            item.Y.ShouldBe(2);
 
             item = path[0];
-            item.X.Should().Be(4);
-            item.Y.Should().Be(2);
+            item.X.ShouldBe(4);
+            item.Y.ShouldBe(2);
            
         }
 
@@ -163,47 +160,45 @@ namespace AStar.Tests
             }
         }
 
-        [Fact]
+        [Test]
         public void ShouldPathAroundObstacle()
         {
-            SetUp();
             _grid[2, 0] = 0;
             _grid[2, 1] = 0;
             _grid[2, 2] = 0;
             _grid[2, 3] = 0;
             var path = _pathFinder.FindPath(new Point(1, 1), new Point(4, 2));
             Helper.Print(_grid, path);
-            path.Count.Should().Be(6);
+            path.Count.ShouldBe(6);
 
             var item = path[5];
-            item.X.Should().Be(1);
-            item.Y.Should().Be(1);
+            item.X.ShouldBe(1);
+            item.Y.ShouldBe(1);
 
             item = path[4];
-            item.X.Should().Be(1);
-            item.Y.Should().Be(2);
+            item.X.ShouldBe(1);
+            item.Y.ShouldBe(2);
 
             item = path[3];
-            item.X.Should().Be(1);
-            item.Y.Should().Be(3);
+            item.X.ShouldBe(1);
+            item.Y.ShouldBe(3);
 
             item = path[2];
-            item.X.Should().Be(2);
-            item.Y.Should().Be(4);
+            item.X.ShouldBe(2);
+            item.Y.ShouldBe(4);
 
             item = path[1];
-            item.X.Should().Be(3);
-            item.Y.Should().Be(3);
+            item.X.ShouldBe(3);
+            item.Y.ShouldBe(3);
 
             item = path[0];
-            item.X.Should().Be(4);
-            item.Y.Should().Be(2);
+            item.X.ShouldBe(4);
+            item.Y.ShouldBe(2);
         }
 
-        [Fact]
+        [Test]
         public void ShouldFindNoPath()
         {
-            SetUp();
             _grid[2, 0] = 0;
             _grid[2, 1] = 0;
             _grid[2, 2] = 0;
@@ -213,7 +208,7 @@ namespace AStar.Tests
             _grid[2, 6] = 0;
             _grid[2, 7] = 0;
             var path = _pathFinder.FindPath(new Point(1, 1), new Point(4, 2));
-            path.Should().BeNull();
+            path.ShouldBe(null);
         }
 
 
