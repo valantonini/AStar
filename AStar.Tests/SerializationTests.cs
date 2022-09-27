b9deba931997c85c50eab24c9c90103252b33f68
val antonini <val.antonini@protonmail.com>
2022-09-28 08:23:31 +1000

    serialization support for pathfinder

--- AStar.Tests/SerializationTests.cs
+++ AStar.Tests/SerializationTests.cs
@@ -1,5 +1,7 @@
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using AStar.Heuristics;
+using AStar.Options;
 using NUnit.Framework;
 using Shouldly;
 
@@ -8,7 +10,7 @@ namespace AStar.Tests
     [TestFixture]
     public class SerializationTests
     {
-        private WorldGrid _world;
+        private PathFinder _pathfinder;
 
         [SetUp]
         public void SetUp()
@@ -17,20 +19,34 @@ namespace AStar.Tests
                           34
                           56";
 
-            _world = Helper.ConvertStringToPathfinderGrid(level);
+            var world = Helper.ConvertStringToPathfinderGrid(level);
+
+            var formatter = new BinaryFormatter();
+            var stream = new System.IO.MemoryStream();
+            var options = new Options.PathFinderOptions
+            {
+                UseDiagonals = false,
+                HeuristicFormula = Heuristics.HeuristicFormula.DiagonalShortCut,
+                PunishChangeDirection = true,
+                SearchLimit = 314,
+            };
+
+            var pathfinder = new PathFinder(world, options);
+            formatter.Serialize(stream, pathfinder);
+            stream.Flush();
+
+            stream.Seek(0, SeekOrigin.Begin);
+            formatter = new BinaryFormatter();
+            _pathfinder = formatter.Deserialize(stream) as PathFinder;
         }
 
         [Test]
         public void TestWorldSerialization()
         {
-            var formatter = new BinaryFormatter();  
-            var stream = new System.IO.MemoryStream();  
-            formatter.Serialize(stream, _world);  
-            stream.Flush();
-
-            stream.Seek(0, SeekOrigin.Begin);
-            formatter = new BinaryFormatter();
-            var grid = formatter.Deserialize(stream) as WorldGrid;
+            var grid = _pathfinder
+                            .GetType()
+                            .GetField("_world", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                            .GetValue(_pathfinder) as WorldGrid;
 
             grid.Width.ShouldBe(2);
             grid.Height.ShouldBe(3);
@@ -44,6 +60,31 @@ namespace AStar.Tests
 
             grid[2, 0].ShouldBe((short)5);
             grid[2, 1].ShouldBe((short)6);
+        }
+
+        [Test]
+        public void TestHeuristicSerialization()
+        {
+            var heuristic = _pathfinder
+                                .GetType()
+                                .GetField("_heuristic", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                                .GetValue(_pathfinder);
+
+            heuristic.ShouldBeOfType<DiagonalShortcut>();
+        }
+
+        [Test]
+        public void TestOptionsSerialization()
+        {
+            var options = _pathfinder
+                        .GetType()
+                        .GetField("_options", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                        .GetValue(_pathfinder) as PathFinderOptions;
+
+            options.UseDiagonals.ShouldBeFalse();
+            options.HeuristicFormula.ShouldBe(HeuristicFormula.DiagonalShortCut);
+            options.PunishChangeDirection.ShouldBeTrue();
+            options.SearchLimit.ShouldBe(314);
         }
     }
 }
